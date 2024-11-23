5a36c06a3649b03b2c1a8cb4be0b71366a11980e
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-23 16:40:31 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+	const int SIZE = 5;
+	int pentagon[SIZE];
+	int result = 0;
+	for (int i = 0; i < SIZE; i++) {
+		cout << "Enter a size: ";
+		cin >> pentagon[i];
+		result += pentagon[i];
+	}
+	cout << "The perimeter of the pentagon is: " << result << endl;
 	return 0;
 }
