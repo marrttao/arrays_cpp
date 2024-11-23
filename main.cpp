892b193693b72fcc5d9a752aca2d17041fccedb2
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-23 16:19:38 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
+	const int SIZE = 10;
+	int array[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	for (int i = SIZE -1; i >= 0; i--) {
+		cout << array[i] << endl;
+	}
 	return 0;
 }
