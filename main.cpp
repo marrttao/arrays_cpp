2b459411bf1ff0c36323d0c3c6670790a11347dd
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-23 16:11:25 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	int income[6] {};
+	int result = 0;
+	for (int i = 0; i <= 5; i++) {
+		cout << "Enter income for month " << i + 1 << ": ";
+		cin >> income[i];
+	}
+	for (int i = 0; i <= 5; i++) {
+		result += income[i];
+	}
+	cout << "Total income for the year is: " << result << endl;
+
 	return 0;
 }
